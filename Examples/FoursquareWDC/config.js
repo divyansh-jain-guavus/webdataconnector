1e73a73928c70373cdbea319eaf409fa7608a1b2
Divyansh Jain <divyansh.jain@thalesgroup.com>
2020-09-21 19:38:37 +0530

    keycloak web data connectors

---
+++ Examples/FoursquareWDC/config.js
@@ -0,0 +1,10 @@
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'CLIENT_ID': 'demo_app',
+ 'CLIENT_SECRET': '283f2c11-4819-4729-8712-1f2eb3d98e76',
+ 'REDIRECT_PATH': '/redirect'
+};
